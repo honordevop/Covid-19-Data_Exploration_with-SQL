45b74291eeaea496b8c428768be8580e2fe14894
honordevop <ogunladestephen20@gmail.com>
2022-06-21 03:29:12 +0100

    Query: returns global death percentage statistics

--- Covid-19_Data_Exploration.sql
+++ Covid-19_Data_Exploration.sql
@@ -99,3 +99,20 @@ GROUP BY
     continent
 order by
     2 DESC;
+
+
+--Checkout global numbers
+SELECT
+    date,
+    SUM(new_cases) as total_cases,
+    SUM(CAST(new_deaths as int)) as total_deaths,
+    SUM(CAST(new_deaths as int)) / SUM(New_cases) * 100 as Deathpercentage
+from
+    ProjectPortfolio..CovidDeaths
+where
+    continent is not null
+GROUP BY
+    date
+ORDER BY
+    1,
+    2;
